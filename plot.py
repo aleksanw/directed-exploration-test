33a92c95bae62e266b46bdbd9afc6cbd3ee86fc3
Aleksander Wasaznik <aleksander@wasaznik.com>
2018-05-20 11:42:48 +0200

    Plotting function based on seaborn

---
+++ plot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def distribution(title, xlabel, data):
+    fig = plt.figure()
+    ax = fig.gca(
+            title=title,
+            xlabel=xlabel,
+            ylabel="Projability density"
+            )
+    for series_label, series_data in data.items():
+        # Plot a filled kernel density estimate
+        sns.distplot(series_data, label=series_label,
+                ax=ax, hist=False, kde_kws={"shade": True})
+    return fig
